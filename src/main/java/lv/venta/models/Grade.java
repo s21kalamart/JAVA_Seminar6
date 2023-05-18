e8e4b79f48570a288fa4088933b32054c750b49a
localuser <localuser@VEA.LV>
2023-05-18 14:35:34 +0300

    Add: new link between Student and Grade

--- src/main/java/lv/venta/models/Grade.java
+++ src/main/java/lv/venta/models/Grade.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -32,5 +34,9 @@ public class Grade {
 	@Min(value = 0)
 	@Max(value = 10)
 	private int gvalue;
+	
+	@ManyToOne
+	@JoinColumn(name = "Ids") 
+	private Student student;
 	
 }
--- src/main/java/lv/venta/models/Student.java
+++ src/main/java/lv/venta/models/Student.java
@@ -1,17 +1,19 @@
 package lv.venta.models;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -19,7 +21,6 @@ import lombok.ToString;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @ToString
 public class Student {
 	
@@ -40,10 +41,8 @@ public class Student {
 	@Size(min = 3, max = 20)
 	@Pattern(regexp = "[A-Z]{1}[a-z]+")
 	private String surname;
-
-	public Student(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
+	
+	@OneToMany(mappedBy = "student")
+	private Collection<Grade> grades;
 	
 }
