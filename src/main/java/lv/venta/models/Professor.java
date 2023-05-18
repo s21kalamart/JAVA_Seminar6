b7a7c6b26a35c899a27f3c91ab20ddf937d50dee
localuser <localuser@VEA.LV>
2023-05-18 14:01:50 +0300

    Add: new model class Professor

---
+++ src/main/java/lv/venta/models/Professor.java
@@ -0,0 +1,54 @@
+package lv.venta.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "professor_table")	//table in DB
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Professor {
+	
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idp;
+	
+	@Column(name = "Name")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String name;
+	
+	@Column(name = "Surname")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String surname;
+	
+	@Column(name = "Degree")
+	@NotNull
+	private Degree degree;
+
+	public Professor(String name, String surname, Degree degree) {
+		this.name = name;
+		this.surname = surname;
+		this.degree = degree;
+	}
+	
+}
