7358ca28dbaec56680f0dc5ab98025218afa56ce
localuser <localuser@VEA.LV>
2023-05-18 14:19:29 +0300

    Add: new model class Grade

--- src/main/java/lv/venta/models/Grade.java
+++ src/main/java/lv/venta/models/Grade.java
@@ -8,38 +8,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Table(name = "course_table")    //table in DB
+@Table(name = "grade_table")    //table in DB
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
-public class Course {
+public class Grade {
 	
-	@Column(name = "Idc")
+	@Column(name = "Idg")
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
-	private long idc;
+	private long idg;
 	
-	@Column(name = "Title")
-	@NotNull
-	@Size(min = 3, max = 20)
-	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+")
-	private String title;
-	
-	@Column(name = "Creditpoints")
-	@Min(value = 1)
-	@Max(value = 20)
-	private int creditpoints;
+	@Column(name = "Gvalue")
+	@Min(value = 0)
+	@Max(value = 10)
+	private int gvalue;
 	
 }
