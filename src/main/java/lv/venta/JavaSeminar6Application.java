f907fc58621d67d31774bcbc702a50ebbc248466
unknown <localuser@vea.lv>
2023-05-18 13:20:33 +0300

    Add: new project for Seminar 6

---
+++ src/main/java/lv/venta/JavaSeminar6Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeminar6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSeminar6Application.class, args);
+	}
+
+}
