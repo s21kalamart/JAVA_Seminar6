021391fe84859486e276d7f048ca768e6ce27f2e
localuser <localuser@VEA.LV>
2023-05-18 16:19:11 +0300

    Add: new ManyToMany link between Professor and Course

--- src/main/java/lv/venta/models/Course.java
+++ src/main/java/lv/venta/models/Course.java
@@ -8,8 +8,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -47,9 +47,9 @@ public class Course {
 	@Max(value = 20)
 	private int creditpoints;
 	
-	@OneToOne
-	@JoinColumn(name = "Idp")
-	private Professor professor;
+	@ManyToMany(mappedBy = "courses")
+	@ToString.Exclude
+	private Collection<Professor> professors;
 	
 	@OneToMany(mappedBy = "course")
 	@ToString.Exclude
--- src/main/java/lv/venta/models/Professor.java
+++ src/main/java/lv/venta/models/Professor.java
@@ -1,11 +1,15 @@
 package lv.venta.models;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -46,9 +50,9 @@ public class Professor {
 	@NotNull
 	private Degree degree;
 	
-	@OneToOne(mappedBy = "professor")
-	@ToString.Exclude
-	private Course course;
+	@ManyToMany
+	@JoinTable(name = "prof_course_table", joinColumns = @JoinColumn(name = "Idc"), inverseJoinColumns = @JoinColumn(name = "Idp"))
+	private Collection<Course> courses;
 
 	public Professor(String name, String surname, Degree degree) {
 		this.name = name;
