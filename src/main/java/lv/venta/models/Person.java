848c6fe6083c24e2e0d1ece0bef710a565bc42b0
localuser <localuser@VEA.LV>
2023-05-25 16:33:00 +0300

    Add: new person model class

---
+++ src/main/java/lv/venta/models/Person.java
@@ -0,0 +1,49 @@
+package lv.venta.models;
+
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@MappedSuperclass
+public class Person {
+	
+	@Column(name = "Idp")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idp;
+	
+	@Column(name = "Name")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String name;
+	
+	@Column(name = "Surname")
+	@NotNull
+	@Size(min = 3, max = 20)
+	@Pattern(regexp = "[A-Z]{1}[a-z]+")
+	private String surname;
+	
+	public Person(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	
+}
